0dd75987579aeaf0d0a933b3b962527d1f17bf03
mladen <mladen.biz@outlook.com>
2020-03-21 16:52:10 +0100

    static root

--- website/website/settings.py
+++ website/website/settings.py
@@ -153,6 +153,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
 STATICFILES_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
